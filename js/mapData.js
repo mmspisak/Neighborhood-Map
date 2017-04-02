bd6062da2c98ba0e597e08ea9c0dd6d10c964d3b
Megan Spisak <mmspisak@comcast.net>
2017-04-01 17:25:28 -0700

    Create mapData.js

---
+++ js/mapData.js
@@ -0,0 +1,26 @@
+
+$("#mapDiv").append(googleMap);
+
+var favPlaces = {
+	"places": [
+		{
+		"name" : "Sushi Ya",
+		"location" : "261 S Roselle Rd, Schaumburg, IL 60193"
+		},
+		{
+		"name" : "Wildberry",
+		"location" : "1383 N Meacham Rd, Schaumburg, IL 60173"
+		},
+		{
+		"name" : "Anna Shea Chocolates & Lounge",
+		"location" : "100 W Higgins Rd, South Barrington, IL 60010"
+		},
+		{
+		"name" : "Walker Bros. Original Pancake House",
+		"location" : "825 Dundee Road, Arlington Hts, IL 60004"
+		},
+		{
+		"name" : "Real Urban Barbecue",
+		"location" : "1260 S Milwaukee Ave, Vernon Hills, IL 60061"
+		}]
+}
